5b3e4d4d981e11527d8f6feefae304b40bbc6ca7
Vlad Prodan <vladprodan6@gmail.com>
2023-05-04 05:55:10 +0200

    paths, verify all

--- libs/web/data-access-axios/src/lib/instance/index.ts
+++ libs/web/data-access-axios/src/lib/instance/index.ts
@@ -1,29 +1,27 @@
-import axios, { type AxiosInstance } from 'axios';
-import {AxiosConstructorParams} from "./types";
+import axios, { type AxiosInstance } from 'axios'
+import { AxiosConstructorParams } from './types'
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: 'https://example.com/api',
   timeout: 5000,
-  headers: { 'Content-Type': 'application/json' },
-});
-
+  headers: { 'Content-Type': 'application/json' }
+})
 
 export class MainApi {
-  private baseUrl: string;
-  private request: AxiosInstance;
-  
+  private baseUrl: string
+  private request: AxiosInstance
+
   constructor(options: AxiosConstructorParams) {
     this.baseUrl = options.baseUrl
-    
+
     this.request = axios.create({
       baseURL: options.baseUrl,
       timeout: 5000,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
-  
-  public getUsers() {
-    return this.request.get("/users")
+      headers: { 'Content-Type': 'application/json' }
+    })
   }
 
+  public getUsers() {
+    return this.request.get('/users')
+  }
 }
